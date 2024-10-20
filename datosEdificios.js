3c7d7dbadd324c98c7e5d6c14edcf7b0fe5e580d
AngelYAvilaA <AngelAvilaArellanes@gmail.com>
2024-10-19 19:18:17 -0600

    Se agrego el script para abrir la etiqueta dialog donde se muestrar la informacion de los edificios y se creo el archivo datosEdificios.js donde se encuentra toda la informacion de los edificios

---
+++ datosEdificios.js
@@ -0,0 +1,164 @@
+const datosEdificios = {
+'edificios700': {
+    titulo: 'Sistemas Computacionales',
+    subtitulo: 'Edificio 700',
+    descripcion: 'La carrera de sistemas computacionales se enfoca en el diseño, desarrollo y gestión de software y hardware. Los profesionales en este campo crean soluciones tecnológicas para resolver problemas complejos, optimizar procesos y mejorar la eficiencia en diversas industrias.',
+    imagen: 'Img/'
+},
+'edificios800': {
+    titulo: 'Arquitectura',
+    subtitulo: 'Edificios 800',
+    descripcion: 'La carrera de arquitectura combina arte y ciencia, permitiendo diseñar espacios funcionales y estéticamente agradables. Los arquitectos crean soluciones innovadoras para mejorar la calidad de vida y el entorno urbano.',
+    imagen: 'Img/arquitecturaITA.jpeg'
+},
+'edificios1000': {
+    titulo: 'Electromecánica',
+    subtitulo: 'Edificios 1000',
+    descripcion: 'La carrera de electromecánica combina arte y ciencia, permitiendo diseñar espacios funcionales y estéticamente agradables. Los arquitectos crean soluciones innovadoras para mejorar la calidad de vida y el entorno urbano.',
+    imagen: 'Img/'
+},
+'CentroDeInformacion': {
+    titulo: 'Centro de Información',
+    subtitulo: 'Centro de Información',
+    descripcion: '',
+    imagen: 'Img/'
+},
+'PostGrado': {
+    titulo: 'PostGrado',
+    subtitulo: 'PostGrado',
+    descripcion: '',
+    imagen: 'Img/'
+},
+'Sindicato': {
+    titulo: 'Sindicato',
+    subtitulo: 'Sindicato',
+    descripcion: '',
+    imagen: 'Img/'
+},
+'EdificioAdministrativo': {
+    titulo: 'Edificio Administrativo',
+    subtitulo: 'Edificio Administrativo',
+    descripcion: '',
+    imagen: 'Img/'
+},
+'Coordinacion': {
+    titulo: 'Coordinación',
+    subtitulo: 'Coordinación',
+    descripcion: '',
+    imagen: 'Img/'
+},
+'CienciasBasicas': {
+    titulo: 'Departamento de Ciencias Básicas',
+    subtitulo: 'Departamento de Ciencias Básicas',
+    descripcion: '',
+    imagen: 'Img/'
+},
+'LVCienciasBasicS': {
+    titulo: 'Laboratorio Virtual de Ciencias Básicas',
+    subtitulo: 'Laboratorio Virtual de Ciencias Básicas',
+    descripcion: '',
+    imagen: 'Img/'
+},
+'SaladeMaestros': {
+    titulo: 'Sala de Maestros',
+    subtitulo: 'Sala de Maestros',
+    descripcion: '',
+    imagen: 'Img/'
+},
+'LaboratorioEaDistancia': {
+    titulo: 'Laboratorio de Educación a Distancia',
+    subtitulo: 'Laboratorio de Educación a Distancia',
+    descripcion: '',
+    imagen: 'Img/'
+},
+'Cafeteria': {
+    titulo: 'Cafetería',
+    subtitulo: 'Cafetería',
+    descripcion: '',
+    imagen: 'Img/'
+},
+'Extraescolares': {
+    titulo: 'Extraescolares',
+    subtitulo: 'Extraescolares',
+    descripcion: '',
+    imagen: 'Img/'
+},
+'LaboratorioElectromecanica': {
+    titulo: 'Laboratorio de Electromecánica',
+    subtitulo: 'Laboratorio de Electromecánica',
+    descripcion: '',
+    imagen: 'Img/'
+},
+'AlmacenTallerLaboratorio': {
+    titulo: 'Almacén, Taller de Mantenimiento y Laboratorio de Arquitectura',
+    subtitulo: 'Almacén, Taller de Mantenimiento y Laboratorio de Arquitectura',
+    descripcion: '',
+    imagen: 'Img/'
+},
+'TallerMaquinasyAlimentos': {
+    titulo: 'Taller de Máquinas y Taller de Alimentos',
+    subtitulo: 'Taller de Máquinas y Taller de Alimentos',
+    descripcion: '',
+    imagen: 'Img/'
+},
+'LaboratorioMyAI': {
+    titulo: 'Laboratorio de Microbiología y Laboratorio de Análisis Instrumentales',
+    subtitulo: 'Laboratorio de Microbiología y Laboratorio de Análisis Instrumentales',
+    descripcion: '',
+    imagen: 'Img/'
+},
+'Edificio600yServicioMedico': {
+    titulo: 'Edificio 600 y Servicio Médico',
+    subtitulo: 'Edificios 600 y Servicio Médico',
+    descripcion: '',
+    imagen: 'Img/'
+},
+'edificios900': {
+    titulo: 'Electromecánica',
+    subtitulo: 'Edificios 900',
+    descripcion: 'La carrera de arquitectura combina arte y ciencia, permitiendo diseñar espacios funcionales y estéticamente agradables. Los arquitectos crean soluciones innovadoras para mejorar la calidad de vida y el entorno urbano.',
+    imagen: 'Img/'
+},
+'edificios300': {
+    titulo: 'Edificios Comodín',
+    subtitulo: 'Edificios 300',
+    descripcion: 'La carrera de arquitectura combina arte y ciencia, permitiendo diseñar espacios funcionales y estéticamente agradables. Los arquitectos crean soluciones innovadoras para mejorar la calidad de vida y el entorno urbano.',
+    imagen: 'Img/'
+},
+'edificios200': {
+    titulo: 'Contaduría',
+    subtitulo: 'Edificios 200',
+    descripcion: 'La carrera de arquitectura combina arte y ciencia, permitiendo diseñar espacios funcionales y estéticamente agradables. Los arquitectos crean soluciones innovadoras para mejorar la calidad de vida y el entorno urbano.',
+    imagen: 'Img/'
+},
+'edificios400': {
+    titulo: 'Gestión Empresarial',
+    subtitulo: 'Edificios 400',
+    descripcion: 'La carrera de arquitectura combina arte y ciencia, permitiendo diseñar espacios funcionales y estéticamente agradables. Los arquitectos crean soluciones innovadoras para mejorar la calidad de vida y el entorno urbano.',
+    imagen: 'Img/'
+},
+'edificios500': {
+    titulo: 'Bioquímica',
+    subtitulo: 'Edificios 500',
+    descripcion: 'La carrera de arquitectura combina arte y ciencia, permitiendo diseñar espacios funcionales y estéticamente agradables. Los arquitectos crean soluciones innovadoras para mejorar la calidad de vida y el entorno urbano.',
+    imagen: 'Img/'
+},
+'edificiosLaborCompu': {
+    titulo: 'Laboratorio de Cómputo',
+    subtitulo: 'Edificios Laboratorio de Cómputo',
+    descripcion: 'La carrera de arquitectura combina arte y ciencia, permitiendo diseñar espacios funcionales y estéticamente agradables. Los arquitectos crean soluciones innovadoras para mejorar la calidad de vida y el entorno urbano.',
+    imagen: 'Img/'
+},
+'edificiosLaborQuimi': {
+    titulo: 'Laboratorio de Química',
+    subtitulo: 'Laboratorio de Química',
+    descripcion: 'La carrera de arquitectura combina arte y ciencia, permitiendo diseñar espacios funcionales y estéticamente agradables. Los arquitectos crean soluciones innovadoras para mejorar la calidad de vida y el entorno urbano.',
+    imagen: 'Img/'
+},
+'edificiosLaborinvesibq': {
+    titulo: 'Laboratorio de Investigación Bioquímica',
+    subtitulo: 'Laboratorio de Investigación Bioquímica',
+    descripcion: 'La carrera de arquitectura combina arte y ciencia, permitiendo diseñar espacios funcionales y estéticamente agradables. Los arquitectos crean soluciones innovadoras para mejorar la calidad de vida y el entorno urbano.',
+    imagen: 'Img/'
+} 
+    };
